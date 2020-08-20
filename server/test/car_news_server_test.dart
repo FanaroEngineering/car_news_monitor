50db4a96bd1cc0dee44e133941df8cc20247c306
Philippe Fanaro <philippefanaro@gmail.com>
2020-08-20 10:33:35 -0300

    fixed pubspecs and imports

--- server/test/car_news_server_test.dart
+++ server/test/car_news_server_test.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:mock_web_server/mock_web_server.dart';
 import 'package:path/path.dart' as p;
 
